5b21981fa9f0fdca40e1ee9dc805572d31f66f4c
lux-jwang <jwang_vip@163.com>
2015-09-04 16:21:35 +0200

    improve performance

--- exp_icdm.py
+++ exp_icdm.py
@@ -31,16 +31,19 @@ def get_predict(u_id, i_id, model, mat_delta,mat_phi):
     item_ids = list(model.get_items(u_id))
     nom = 0
     denom = 0
+    pre_delta_xa = mat_delta[i_id]
+    pre_phi_xa = mat_phi[i_id]
+    pre_ratings = model.raw_data[u_id]
+
     for a_id in item_ids:
         if a_id == i_id:
             continue
-        delta_xa = mat_delta[i_id][a_id]
-        phi_xa = mat_phi[i_id][a_id]
+        delta_xa = pre_delta_xa[a_id] #mat_delta[i_id][a_id]
+        phi_xa = pre_phi_xa[a_id] #mat_phi[i_id][a_id]
 
         if delta_xa == -1000 or phi_xa == 0: #avoid such a case
             continue
-
-        r_ua = model.get_rate(u_id,a_id,ts=False)
+        r_ua, _ = pre_ratings[a_id] #model.get_rate(u_id,a_id,ts=False)
 
         nom += (delta_xa+r_ua)*phi_xa
         denom += phi_xa
@@ -48,7 +51,7 @@ def get_predict(u_id, i_id, model, mat_delta,mat_phi):
     if denom < 1:
         return 0
     
-    return float(nom)/float(denom)
+    return nom/float(denom)
 
 
 def get_matrix_predict(model,mat_delta,mat_phi):
